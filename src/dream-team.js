47fb11943e401cc95216fdeea4ed7210b0b1ed85
Viktor Gubarevich <gubarew.v@gmail.com>
2021-03-27 23:24:13 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
+  let nameDreamTeam = '';
+
+  if (!Array.isArray(members)) {
+    return false
+  } else {
+    members.forEach(elem => {
+      if (typeof (elem) === 'string') {
+        nameDreamTeam += elem.trim().slice(0, 1).toUpperCase()
+      }
+    })
+  }
+  return nameDreamTeam.split('').sort().join('')
+
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
 };
