46632fe28f0488e017552f35371dd9ce1eacf93a
Viktor Gubarevich <gubarew.v@gmail.com>
2021-03-27 19:34:32 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const natLogarithm = 0.693;
+const sampleNum = Number(sampleActivity);
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
+
+  if (Math.ceil(Math.exp(MODERN_ACTIVITY / sampleNum) / (natLogarithm / sampleNum))) {
+    return true
+  } else if ((typeof (sampleNum) !== Number || sampleActivity === "")) {
+    return false
+  };
+
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
 };
