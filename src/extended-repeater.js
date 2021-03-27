e9b04b85d18bb21cbf21cd0c9ab6c59494404be0
Viktor Gubarevich <gubarew.v@gmail.com>
2021-03-28 00:10:24 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
+module.exports = function repeater(str, options) {
+
+  let {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+  } = options;
+
+  let result = [];
+
+  separator = separator || '+';
+  additionSeparator = additionSeparator || '|';
+
+  if (addition === null) {
+    addition = 'null'
+  } else {
+    addition = addition
+  };
+
+  result = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+  return new Array(repeatTimes).fill(str + result).join(separator);
+
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
 };
-  
