79095df2fec9a7989e1decd761d1a47539e540ce
Viktor Gubarevich <gubarew.v@gmail.com>
2021-03-29 21:25:50 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
+  encrypt(message, key) {
+    this.value(message, key);
+    return this.getResult(message, key);
+    throw new CustomError('Not implemented');
+  }
+  decrypt(encryptedMessage, key) {
+    this.value(encryptedMessage, key);
+    return this.getResult(encryptedMessage, key, true);
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  }
+
+  constructor(valueBoolean) {
+    this.valueBoolean = valueBoolean === false;
+    this.az = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  }
+
+  value(message, key) {
+    if (typeof message === "undefined" || typeof key === "undefined") {
+      throw new Error()
+    };
   }
 }
 
